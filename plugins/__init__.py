03ab5d502dea79ef8689bd9d232f94a27560f464
Huy Dinh <phradion@gmail.com>
2013-07-19 15:19:30 +1000

    easy hack to get Phrangman loaded

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3,6 +3,6 @@ import glob
 
 
 __all__ = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__) + '/*.py')]
-
+from Phrangman import Phrangman
 for module in __all__:
     __import__(module, locals(), globals())
