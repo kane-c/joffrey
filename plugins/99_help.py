07d667aa9a854b542b726407338bfe8f444b6ce3
Kane <kane@smsglobal.com>
2013-07-19 15:40:40 +1000

    Help plugin

---
+++ plugins/99_help.py
@@ -0,0 +1,20 @@
+from registry import BasePlugin, plugin_registry
+
+
+class Help(BasePlugin):
+    name = 'Help'
+    commands = {
+        '!help': 'This help command',
+    }
+
+    def process(self, message, sender):
+        if message.lower()[:5] == '!help':
+            reply = ['Available commands:']
+            for plugin in plugin_registry.plugins:
+                if plugin.commands:
+                    reply.append(plugin.name)
+                    reply.append('\n'.join(('{}: {}'.format(c, d) for c, d in plugin.commands.iteritems())))
+
+            return '\n'.join(reply)
+
+plugin_registry.register(Help())
