15d5e834a283023205a0b1a87100cd8aaf47068b
Huy Dinh <phradion@gmail.com>
2013-07-19 16:06:18 +1000

    Getting there, more aggressive bot and better encapsulation

--- plugins/Phrangman/Phrangman.py
+++ plugins/Phrangman/Phrangman.py
@@ -5,97 +5,126 @@ import os
 import random
 
 class Phrangman(BasePlugin):
-    commands = ('hangman start', 'hangman area', 'hangman stop', 'hangman letter')
-    msg_start = 'Hahaha. You peasants will all be hang if you can\'t get it right.\nNow tell me which area of knowledges you want to challenge by typing "hangman area <name of the area>":\n'
-    knowledge_areas = []
-    count_rules = {'3': 3, '4': 4, 'other': 9}
-    count_rule = 9
-    hangman_images = { }
-    word = ''
-    selected_characters = []
-    available_characters = []
+	
+	count_rules = {'3': 5, '4': 7, 'other': 12}	
+	commands = ('hangman start', 'hangman area', 'hangman stop', 'hangman letter')
     
 	def __init__(self):
-		for knowledge_area in os.listdir('./data/words/'):
-			knowledge_areas += file += ','
-		for hangman_image in os.listdir('./data/hangman'):
-			f = open (hangman_image,"r")
+		self.reset()
+		script_dir = os.path.dirname(__file__)					
+	
+		# Load Words Data
+		words_path = os.path.join(script_dir, 'data/hangman/')		
+		knowledge_areas = []
+		for knowledge_area in os.listdir(words_path):
+			self.knowledge_areas.append(os.path.join(words_path, knowledge_area))
+		hangman_images_path = os.path.join(script_dir, 'data/hangman/')
+		
+		# Load 'graphic' elements ;)
+		self.hangman_images = { }		
+		for hangman_image in os.listdir(hangman_images_path):
+			f = open (os.path.join(hangman_images_path, hangman_image),"r")
 			image = f.read()
-			hangman_images[hangman_image] = image
+			self.hangman_images[hangman_image] = image
 		
 		for i in range(1,26):
-			available_characters = str(unichr(96+i))
-		}
+			self.available_characters = str(unichr(96+i))
 	
+	def reset(self):
+		self.count_rule = 9
+		self.word = ''
+		self.selected_characters = []
+		self.available_characters = []
+		
+	def getCountRule(self):
+		if self.word == '':
+			return -1
+		else:
+			if `self.word.__len__()` in count_rules:
+				return count_rules[`self.word.__len__()`]
+			else
+				return count_rules['other']
+				
 	def getWordMask(self):
 		mask = ''
-		letter_choices = list(word)
+		letter_choices = list(self.word)
 		for letter in letter_choices:
-			if letter in selected_characters:
+			if letter in self.selected_characters:
 				mask += letter.upper()
 			else:
 				mask += '_'
 		return mask
 
 	def getHangProgression(self):
-		progression = round(len(selected_characters) / count_rule) * 100)
-		if (progression > 100)
+		progression = round(len(self.selected_characters) / self.getCountRule()) * 100
+		if progression > 100:
 			progression = 100
-		progression = ((int)progression / 10) * 10
+		progression = (progression//10) * 10
 		return progression
 		
-	def getHangProgressionImage(progression):
-		return hangman_images['10'] + '\n\n'
+	def getHangProgressionImage(self, progression):
+		return self.hangman_images['10'] + '\n\n'
 		
 	def wonYet(self):
 		for l in word:
-			if l not in selected_characters
+			if l not in self.selected_characters:
 				return false
 		return true 
 
-    def process(self, message, sender):
-        if 'hangman start' == message.lower():
-        	msg_start = msg_start
-        	for area in knowledge_areas:
-				return_msg += self.knowledge_areas += ','
+	def process(self, message, sender):
+		if 'hangman start' == message.lower():
+			msg_start = 'Hahaha. You peasants will all be hang if you can\'t get it right.\nNow tell me which area of knowledges you want to challenge by typing "hangman area <name of the area>":\n'
+			for area in knowledge_areas:
+				return_msg += area + ','
 			return start_msg        	
         	
 		elif 'hangman area' in message.lower():				
-        	message_words = message.lower().split()
-        	area_name = message_words[2] 
-        	if getHangProgression() < 100 :
+			message_words = message.lower().split()
+			area_name = message_words[2] 
+        	if self.getHangProgression() < 100 :
         		return 'You haven\'t solved the other challange yet you stupid peasants.'
-
-            if area_name in knowledge_areas:
-                msg_area = getHangProgressionImage()
-				lines = open('./data/words/'+area_name).read().splitlines()
-				word = random.choice(lines)
-				mask = getWordMask()
+			
+			if area_name in knowledge_areas:
+				lines = open('./data/words/' + area_name).read().splitlines()			
+				self.word = random.choice(lines)
+				msg_area = self.getHangProgressionImage()
+				mask = self.getWordMask()
 				msg_area += 'Solve this: ' + mask
 				
 				return msg_area
-				
-            else:
-            	return 'Can\'t you read you peasant, well I guess not. Pick one of the areas that I have said above ^'
+			else:
+				return 'Can\'t you read you peasant, well I guess not. Pick one of the areas that I have said above ^'
+
 		elif 'hangman letter' in message.lower():
-        	message_words = message.lower().split()
-        	guess = message_words[2] 
-        	if guess not in available_characters:
+			message_words = message.lower().split()
+			guess = message_words[2] 
+        	if guess not in self.available_characters:
         		return 'Hey, in my kingdom you are only allowed to use ASCHII characters.'
         		
-        	if guess in selected_characters:
+        	if guess in self.selected_characters:
         		return 'I guess you peasant has a short-term memory issue. You have already guessed that character before.'
-        	else
-    	    	selected_characters.append(guess)
-	        	if wonYet():
-	        		return 'CONGRATULATION! I KNOW PROMOTE YOU TO BECOME THE HAND OF THE KING!'
-
-				if displayHangProgression
-	        		
-
-        			
-        	
-    def post_process(self, reply, message, sender):
-        return '{}, peasant'.format(reply)
+       		else:
+       			self.selected_characters.append(guess)
+       			if wonYet():
+       				return 'CONGRATULATION! I KNOW PROMOTE YOU TO BECOME THE HAND OF THE KING!'
+				
+				progression = self.getHangProgression()
+				game_progress_msg = self.getHangProgressionImage(progression)
+				
+				if progression == 100:
+					game_progress_msg += 'HAHAHA! GAME ENDED! YOU SHALL BE SERVED IDIOT!\n*CHOPPING SOUND*'
+					self.reset()
+				else:
+					mask = self.getWordMask()
+					msg_area += 'So far: ' + mask
+				
+				return game_progress_msg
+		
+		elif 'hangman stop' in message.lower():
+			self.reset()
+			return 'SHAME ON YOU PEASANT! You dared to quit this Godly made game'
+			
+	def post_process(self, reply, message, sender):
+		return '{}, peasant'.format(reply)
 
 plugin_registry.register(Phrangman())
